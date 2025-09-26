6d177c4743e4955cd1856e94e2a725b265848026
mccullrs <mccullrs@rose-hulman.edu>
2025-09-26 19:26:19 -0400

    maybe gen works

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+window.addEventListener("load", (e) => {
+    navBar =   "<center>\
+                    <nav id=\"nav-bar\">\
+                        <button><a href=\"index.html\" aria-label=\"Main page\">Main page</a></button>\
+                        <button><a href=\"resume.html\" aria-label=\"Resume page\">Resume page</a></button>\
+                        <button><a href=\"portfolio.html\" aria-label=\"Portfolio page\">Portfolio page</a></button>\
+                    </nav>\
+                </center>";
+    
+    footer =   "<footer>\
+                    <nav>\
+                        <button><a href=\"https://app.joinhandshake.com/profiles/bu5wmu\">Handshake</a></button>\
+                        <button><a href=\"www.linkedin.com/in/riley-mcculloch-170114387\">LinkedIn</a></button>\
+                    </nav>\
+ 	            </footer>";
+    
+    main = document.getElementsByTagName("main");
+    body = document.getElementsByTagName("body");
 
+    main.innerHTML = navBar + main.innerHTML;
+    body.innerHTML = body.innerHTML + footer;
+
+
+    
+});
