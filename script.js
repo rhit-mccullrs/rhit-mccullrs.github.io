9c4e6ad451df36cdada51191f84fa99572ebbb70
mccullrs <mccullrs@rose-hulman.edu>
2025-09-28 15:27:58 -0400

    removed wait on js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", (e) => {
+//window.addEventListener("load", (e) => {
     navBar =   "<section>\
                     <center>\
                         <nav id=\"nav-bar\">\
@@ -23,4 +23,4 @@ window.addEventListener("load", (e) => {
 
     main.innerHTML = navBar + main.innerHTML;
     body.innerHTML = body.innerHTML + footer;
-});
+//});
