a534be9fb8d7002863bdf49205e387fa338fa06b
mccullrs <mccullrs@rose-hulman.edu>
2025-09-29 03:37:15 -0400

    huge fixes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", (e) => {
     navBar =   "<section>\
                     <center>\
-                        <nav id=\"nav-bar\">\
-                            <ul>\
-                                <li><a class=\"nav-button\" href=\"index.html\" aria-label=\"Main page\">Main</a></li>\
-                                <li><a class=\"nav-button\" href=\"resume.html\" aria-label=\"Resume page\">Resume</a></li>\
-                                <li><a class=\"nav-button\" href=\"portfolio.html\" aria-label=\"Portfolio page\">Portfolio</a></li>\
+                        <nav>\
+                            <ul id=\"nav-bar\" class=\"fixed\">\
+                                <li><a class=\"button\" href=\"index.html\" aria-label=\"Main page\">Main</a></li>\
+                                <li><a class=\"button\" href=\"resume.html\" aria-label=\"Resume page\">Resume</a></li>\
+                                <li><a class=\"button\" href=\"portfolio.html\" aria-label=\"Portfolio page\">Portfolio</a></li>\
                             </ul>\
                         </nav>\
                     </center>\
@@ -13,9 +13,11 @@ window.addEventListener("load", (e) => {
     
     footer =   "<footer>\
                     <center>\
-                        <nav id=\"footer\">\
-                            <a class=\"button-links\" href=\"https://app.joinhandshake.com/profiles/bu5wmu\" target=\"_blank\" aria-label=\"Riley's Handshake profile\">Handshake</a>\
-                            <a class=\"button-links\" href=\"https://linkedin.com/in/riley-mcculloch-170114387\" target=\"_blank\" aria-label=\"Riley's LinkedIn profile\">LinkedIn</a>\
+                        <nav>\
+                            <ul id=\"footer\" class=\"fixed\">\
+                                <li><a class=\"button\" href=\"https://app.joinhandshake.com/profiles/bu5wmu\" target=\"_blank\" aria-label=\"Riley's Handshake profile\">Handshake</a></li>\
+                                <li><a class=\"button\" href=\"https://linkedin.com/in/riley-mcculloch-170114387\" target=\"_blank\" aria-label=\"Riley's LinkedIn profile\">LinkedIn</a></li>\
+                            </ul>\
                         </nav>\
                     </center>\
  	            </footer>";
